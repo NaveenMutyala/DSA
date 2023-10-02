e0271c0e971f3b84be506b0dec201f3c05b8c5f9
Naveen kumar Mutyala <81841128+NaveenMutyala@users.noreply.github.com>
2023-10-02 12:18:28 +0530

    Update allpalindromes.py

--- allpalindromes.py
+++ allpalindromes.py
@@ -13,6 +13,21 @@ def palindrome(a,freq):
 freq=[False]*len(arr)
 palindrome([],freq)
 
+# or
+
+arr=[1,2,3]
+N= len(arr)
+def palindrome(ind):
+    if ind==N:
+        print(*arr)
+        return
+    for i in range(ind,N):
+        arr[ind],arr[i]=arr[i],arr[ind]
+        palindrome(ind+1)
+        arr[ind],arr[i]=arr[i],arr[ind]
+  
+palindrome(0)
+
 # 1 2 3
 # 1 3 2
 # 2 1 3
